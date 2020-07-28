56b5cfe8f6ecca545680b6994083ba580c08f305
Ruhi Mahendra <ruhimahendra24@gmail.com>
2020-07-28 09:43:44 -0400

    readme update part 1

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# Vascan
+# VALIDATING TAXA IN FNA WITH TAXA IN VASCAN
 
+## Project
+
+The purpose of this project is to validate the taxa that are present in FNA with those that are present in VASCAN. This will allow the users to determine which taxa are not present in FNA... edit  
+
+## Repository
+
+### Scripts
+
+This repository contains two main scripts:
+
+1) `canada-taxa-files/QUERY-CSV.R`
+
+Download the taxa found in Canada from the FNA API and merge all the taxa names together in text file that can be used to search through the VASCAN API
+
+2) `taxize-vascan-long-data.R` or `taxize-vascan-wide-data.R`
+
+Validate all the names from the newly curated text file against the names from the VASCAN API
+
+### CSV files
+
+
+The output of this project creates a final CSV file that shows the taxa from FNA that shows the "matched" taxa against the taxa in VASCAN
+
+Two different output files are produced depending on if "wide data" (`Canada_Taxa.CSV`) or "long data" (`Canada_Taxa_long.CSV`) is wanted.
+
+Here is a preview of `Canada_Taxa.CSV`:
+
+|Taxon Name          |Match|Number of Matches|Match Number|Matches                         |Scientific Name Authorship|Canonical Name|Taxon Rank|Name According To                                                                                                                                 |Taxonic Status|Higher Classification                                                                     |
+|--------------------|-----|-----------------|------------|--------------------------------|--------------------------|--------------|----------|--------------------------------------------------------------------------------------------------------------------------------------------------|--------------|------------------------------------------------------------------------------------------|
+|Abies balsamea      |yes  |1                |1           |Abies balsamea (Linnaeus) Miller|(Linnaeus) Miller         |Abies balsamea|species   |FNA Editorial Committee. 1993. Flora of North America north of Mexico. Volume 2: Pteridophytes and Gymnosperms. Oxford University Press, New York.|accepted      |Equisetopsida;Pinidae;Pinales;Pinaceae;Abies;Abies sect. Balsamea;Abies subsect. Laterales|
+|Abies bifolia       |yes  |1                |1           |Abies bifolia A. Murray         |A. Murray                 |Abies bifolia |species   |FNA Editorial Committee. 1993. Flora of North America north of Mexico. Volume 2: Pteridophytes and Gymnosperms. Oxford University Press, New York.|accepted      |Equisetopsida;Pinidae;Pinales;Pinaceae;Abies;Abies sect. Balsamea;Abies subsect. Laterales|
+|Abietinella abietina|no   |0                |1           | NA                             |NULL                      |NULL          |NULL      |NULL                                                                                                                                              |NULL          |NULL                                                                                      |
+
+Here is a preview of `Canada_Taxa_long.CSV`:
+
+|Taxon Name          |Match|Variable|Value|
+|--------------------|-----|--------|-----|
+|Abies bifolia       |Abies bifolia A. Murray|Match Number|1    |
+|Abies bifolia       |Abies bifolia A. Murray|Scientific Name Authorship|A. Murray|
+|Abies bifolia       |Abies bifolia A. Murray|Taxon Rank|species|
+|Abies bifolia       |Abies bifolia A. Murray|Name According To|FNA Editorial Committee. 1993. Flora of North America north of Mexico. Volume 2: Pteridophytes and Gymnosperms. Oxford University Press, New York.|
+|Abies bifolia       |Abies bifolia A. Murray|Canonical Name|Abies bifolia|
+|Abies bifolia       |Abies bifolia A. Murray|Taxonomic Status|accepted|
+|Abies bifolia       |Abies bifolia A. Murray|Higher Classification|Equisetopsida;Pinidae;Pinales;Pinaceae;Abies;Abies sect. Balsamea;Abies subsect. Laterales|
+|Abietinella abietina|NA   |NA      |NA   |
--- taxize-vascan-long-data.R
+++ taxize-vascan-long-data.R
@@ -1,19 +1,17 @@
 # uses the "vascan_search" function from the taxize package. 
 # searches all the taxons from FNA to compare presence in vasca
-# Creates CSV file that gives results for each taxon in FNA
+# Creates CSV file (longer data vs. wider data) that gives results for each taxon in FNA
 
 library(taxize)
 library(stringi)
 library(jsonlite)
 library(purrr)
-
-
 processFile = function(filepath){
   #creates header names for csv file
-  column_names <- data.frame(TaxonName = "Taxon Name", Match = "Match", NumberofMatches = "Number of Matches", MatchNumber = "Match Number", Matches = "Matches", ScientificNameAuthorship ="Scientific Name Authorship", CanonicalName = "Canonical Name", TaxonRank = "Taxon Rank", NameAccordingTo = "Name According To", TaxonomicStatus = "Taxonic Status", HigherClassification = "Higher Classification" )
+  column_names <- data.frame(TaxonName = "Taxon Name", Match = "Match", Variable = "Variable", Value = "Value")
   con = file(filepath, "r")
-  while(TRUE) {
-    line = readLines (con, n=1) #loops over every line (taxon) in the text file
+  while(TRUE) { #loops over every line (taxon) in the text file
+    line = readLines (con, n=1)
     print(line)
     if ( length(line) == 0) {
       break
@@ -25,13 +23,7 @@ processFile = function(filepath){
     j_son = jsonlite::fromJSON(j_search) #allows us the extract data from "json" format
     
     nummatches <- v_search[[1]][2][[1]] #provides us with the number of matches
-    if (nummatches > 0) { 
-      is_match <- "yes"
-
-    } else {
-    is_match <- "no"
-    }
-    
+   
     #extracting data from the "list" format
     matches <- v_search[[1]][3][[1]]
     matched_names_list <- map((map(matches, 1)) , 1)
@@ -44,8 +36,8 @@ processFile = function(filepath){
     Canonical_name_list <- j_son$results$matches[[1]][4]
     Taxon_rank_list <- j_son$results$matches[[1]][5]
     
-  
-    n = 0 
+    
+    n = 0
     
     for (i in matched_names_list) { #loop through every matched name
       n = n + 1 #adding 1 for every match, this will help us keep track of each match
@@ -61,23 +53,30 @@ processFile = function(filepath){
       if (class(Higher_classification) == "NULL"){Higher_classification <- "NULL"}
       else {Higher_classification <-higher_classification_list[[n]] }
       
-      # for the taxons that do not have matches, some paramaters must be set to "NULL"
-      if (nummatches < 1) {newline <-data.frame(t(c(TaxonName = line, Match = is_match, NumberofMatches = nummatches, MatchNumber = n ,  Matches = Matched_name, ScientificNameAuthorship = "NULL", CanonicalName = "NULL", TaxonRank = "NULL", NameAccordingTo = "NULL", TaxonomicStatus = "NULL", HigherClassification = "NULL"  )))  }
-      else {newline <- data.frame(t(c(TaxonName = line, Match = is_match, NumberofMatches = nummatches, MatchNumber = n ,  Matches = Matched_name, ScientificNameAuthorship = Scientific_Name_authorship, CanonicalName = Canonical_Name, TaxonRank = Taxon_Rank, NameAccordingTo = Name_According_To, TaxonomicStatus = Taxonomic_status, HigherClassification = Higher_classification  )))}
-      #combine new taxon name to data frame
-      column_names <- rbind(column_names, newline)
+      #to make the formal of the data long, a new row is created for each variable
+      if (nummatches > 0) {newline <-data.frame(t(c(TaxonName = line, Match = Matched_name, Variable = "Match Number", Value = n)))
+      newline2 <- data.frame(t(c(TaxonName = line, Match = Matched_name, Variable = "Scientific Name Authorship", Value = Scientific_Name_authorship)))
+      newline3 <-data.frame(t(c(TaxonName = line, Match = Matched_name, Variable = "Taxon Rank", Value = Taxon_Rank)))
+      newline4 <- data.frame(t(c(TaxonName = line, Match = Matched_name, Variable = "Name According To", Value = Name_According_To)))
+      newline5 <- data.frame(t(c(TaxonName = line, Match = Matched_name, Variable = "Canonical Name", Value = Canonical_Name)))
+      newline6 <-data.frame(t(c(TaxonName = line, Match = Matched_name, Variable = "Taxonomic Status", Value = Taxonomic_status))) 
+      newline7 <-data.frame(t(c(TaxonName = line, Match = Matched_name, Variable = "Higher Classification", Value = Higher_classification))) 
+      column_names <- rbind(column_names, newline, newline2, newline3, newline4, newline5, newline6, newline7) }
+      
+      #if there is no match, only one line is created
+      else {newline <- data.frame(t(c(TaxonName = line, Match = "NA", Variable = "NA", Value = "NA"  )))
+      column_names <- rbind(column_names, newline)}
+      
       
     }
     
     
-  
+    
   }
   close(con)
-  #write final csv file
-  write.table(column_names, file ="/home/ruhimahendra24/Desktop/Vascan/Canada_Taxa.csv", row.names = FALSE, append = FALSE, col.names = FALSE, sep = ", " )
+  write.table(column_names, file ="/home/ruhimahendra24/Desktop/Vascan/Output/Canada_Taxa_long.csv", row.names = FALSE, append = FALSE, col.names = FALSE, sep = ", " )
   
   
 }
 
-#processFile('/home/ruhimahendra24/Desktop/Vascan/canada-taxa-files/canada_taxa.txt')
-
+#processFile('/home/ruhimahendra24/Desktop/Vascan/canada_taxa.txt')

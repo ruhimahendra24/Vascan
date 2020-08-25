9c23515cfdd30ae03d268cbb2404a5e6f627e410
Jocelyn Pender <jpend049@uottawa.ca>
2020-08-25 11:49:44 -0400

    a DRY version of Query_CSV.R

--- canada-taxa-files/Query_CSV.R
+++ canada-taxa-files/Query_CSV.R
@@ -4,22 +4,16 @@ source('./canada-taxa-files/query.R')
 #not including duplicates.
 #Creates text file to use for vascan_search function.
 
-merge_canada <- function(x) { #combine the data frames
-  canada_all <- rbind(ask_query_titles("[[Distribution::Ont.]]", "./canada-taxa-files/Province_Files/ontario_taxa.csv"),
-  ask_query_titles("[[Distribution::Alta.]]", "./canada-taxa-files/Province_Files/alberta_taxa.csv"),
-  ask_query_titles("[[Distribution::B.C.]]", "./canada-taxa-files/Province_Files/britishcolumbia_taxa.csv"),
-  ask_query_titles("[[Distribution::Man.]]", "./canada-taxa-files/Province_Files/manitoba_taxa.csv"),
-  ask_query_titles("[[Distribution::N.B.]]", "./canada-taxa-files/Province_Files/newbrunswick_taxa.csv"),
-  ask_query_titles("[[Distribution::Nfld.]]", "./canada-taxa-files/Province_Files/newfoundland_taxa.csv"),
-  ask_query_titles("[[Distribution::Nfld. and Labr.]]", "./canada-taxa-files/Province_Files/newfoundlandandlabrador_taxa.csv"),
-  ask_query_titles("[[Distribution::N.W.T.]]", "./canada-taxa-files/Province_Files/northwestterritories_taxa.csv"),
-  ask_query_titles("[[Distribution::N.S.]]", "./canada-taxa-files/Province_Files/novascotia_taxa.csv"),
-  ask_query_titles("[[Distribution::Que.]]", "./canada-taxa-files/Province_Files/quebec_taxa.csv"),
-  ask_query_titles("[[Distribution::P.E.I.]]", "./canada-taxa-files/Province_Files/princeedwardisland_taxa.csv"),
-  ask_query_titles("[[Distribution::Nunavut.]]", "./canada-taxa-files/Province_Files/nunavut_taxa.csv"),
-  ask_query_titles("[[Distribution::Sask.]]", "./canada-taxa-files/Province_Files/saskatchewan_taxa.csv"),
-  ask_query_titles("[[Distribution::Yukon.]]", "./canada-taxa-files/Province_Files/yukon_taxa.csv")) 
-  non_duplicate <- distinct(canada_all) #remove duplicates
-  write.table(non_duplicate, "./canada-taxa-files/canada_taxa.txt", row.names = FALSE, col.names = FALSE, quote = FALSE) #create text file
+run_province_query <- function(province = "Ont.") {
+  prov_query_string <- paste("[[Distribution::", province, "]]", sep = "")
+  prov_file_name <- paste("./canada-taxa-files/Province_Files/", province, "_taxa.csv", sep = "")
+  ask_query_titles(prov_query_string, prov_file_name)
 }
 
+merge_canada <- function(provinces = c("Ont.", "Alta.", "B.C.", "Man.", "N.B.", "Nfld.", 
+                                       "Nfld. and Labr.", "N.W.T.", "N.S.", "Que.", 
+                                       "P.E.I.", "Nunavut.", "Sask.", "Yukon.")) { 
+  canada_all <- map(provinces, run_province_query)
+  non_duplicate <- canada_all %>% bind_rows() %>% distinct()
+  write.table(non_duplicate, "./canada-taxa-files/canada_taxa.txt", row.names = FALSE, col.names = FALSE, quote = FALSE) #create text file
+}
